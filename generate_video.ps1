37a50ab1728b71ecad1b851de98f98bb4ead3411
Omar Saad <omar.saad0705@gmail.com>
2025-12-12 15:15:05 +0100

    fix: update ffmpeg command in generate_video.ps1 to use CRF 18 for improved video quality

--- generate_video.ps1
+++ generate_video.ps1
@@ -1,2 +1,2 @@
 Get-ChildItem saved_images/*.png | ForEach-Object { "file 'saved_images/$($_.Name)'" } | Out-File -Encoding ASCII images.txt
-ffmpeg -f concat -safe 0 -r 24 -i images.txt -c:v libx265 -pix_fmt yuv420p output.mp4 -y
+ffmpeg -f concat -safe 0 -r 24 -i images.txt -c:v libx265 -crf 18 -pix_fmt yuv420p output.mp4 -y
