c30496480547033253aae0a03b51fc647616358f
Omar Saad <omar.saad0705@gmail.com>
2025-12-12 15:14:15 +0100

    feat: add video generation script and update .gitignore to include saved images and output files

---
+++ generate_video.ps1
@@ -0,0 +1,2 @@
+Get-ChildItem saved_images/*.png | ForEach-Object { "file 'saved_images/$($_.Name)'" } | Out-File -Encoding ASCII images.txt
+ffmpeg -f concat -safe 0 -r 24 -i images.txt -c:v libx265 -pix_fmt yuv420p output.mp4 -y
